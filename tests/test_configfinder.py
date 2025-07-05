3112eec2769ec4575a35afa2d1a3c9a552e5c36d
Fabi <your@mail.com>
2025-07-05 10:30:55 +0200

    test: mock file structure

--- tests/test_configfinder.py
+++ tests/test_configfinder.py
@@ -1,5 +1,6 @@
 import pytest
 
+import __main__
 from simpleconfigfinder.configfinder import (
     ConfigNotFound,
     config_finder,
@@ -8,11 +9,20 @@ from simpleconfigfinder.configfinder import (
 )
 
 
-def test_file_finder():
-    with pytest.raises(FileNotFoundError):
-        find_file("wrongfile.ext")
+def test_file_finder(tmp_path):
+    fname = "test.toml"
+    file_config = tmp_path / fname
+    file_python = tmp_path / "src" / "some_program.py"
 
-    find_file("pyproject.toml")
+    file_config.write_text("test")
+
+    with pytest.MonkeyPatch.context() as context:
+        context.setattr(__main__, "__file__", file_python)
+
+        with pytest.raises(FileNotFoundError):
+            find_file("wrongfile.ext")
+
+        assert find_file(fname) == file_config
 
 
 def test_config_walker():
@@ -46,6 +56,28 @@ def test_config_walker():
         config_walker(dictionary_test, ["b", "b3"])
 
 
-def test_config_finder():
+def test_config_finder(tmp_path):
+    test_input = [
+        "[a.b1]",
+        "c = 1",
+        "d = 2",
+        "[a.b2]",
+        "c = 10",
+        "d = 22",
+    ]
+    fname = "test.toml"
+    file_config = tmp_path / fname
+    file_python = tmp_path / "some_program.py"
+
+    file_config.write_text("\n".join(s for s in test_input))
+
+    with pytest.MonkeyPatch.context() as context:
+        context.setattr(__main__, "__file__", file_python)
+
+        assert config_finder(fname, ["a", "b1"]) == {
+            "c": 1,
+            "d": 2,
+        }
+
     with pytest.raises(NotImplementedError):
         config_finder("somefile.ext", ["a"])
